c93002af1d02c196f5b94d47184d51e0e8bbc3b3
deyvisongdias <deyvisondias@hotmail.com>
2021-06-08 13:56:02 -0300

    Iniciação das classes

---
+++ Arquivos.cpp
@@ -0,0 +1,45 @@
+#include "Arquivos.h"
+#include "list"
+
+using  namespace  std;
+
+Arquivos::Arquivos() {
+    fstream arquivo;
+    string linha;
+    list<string> listaArtists;
+
+    arquivo.open("artists.csv", ios::in);
+
+    if (arquivo.is_open()) {
+        while (getline(arquivo, linha)) {
+            cout << linha << endl;
+            listaArtists.push_back(linha);
+        }
+        arquivo.close();
+    } else {
+        cout << "Nao foi possivel abrir o arquivo (Arquivo nao esta aberto)" << endl;
+    }
+
+
+//abrindo e inserindo na lista do tracks
+    fstream arquivo2;
+    string linha2;
+    list<string> listaTracks;
+
+    arquivo.open("tracks.csv", ios::in);
+
+    if (arquivo.is_open()) {
+        while (getline(arquivo2, linha2)) {
+            cout << linha2 << endl;
+            listaTracks.push_back(linha2);
+        }
+        arquivo2.close();
+    } else {
+        cout << "Nao foi possivel abrir o arquivo (Arquivo nao esta aberto)" << endl;
+    }
+}
+
+ Arquivos::~Arquivos()
+ {
+
+ }
---
+++ Arquivos.h
@@ -0,0 +1,54 @@
+#include "list"
+#include "date"
+
+#ifndef UNTITLED_ARQUIVOS_H
+#define UNTITLED_ARQUIVOS_H
+
+
+class Arquivos
+{
+private:
+    //atributos do artists
+    String id;
+    float followers;
+    String genres;
+    String name;
+    int popularity;
+
+    //atributos do tracks
+
+    String id2;
+    String name2;
+    int popularity2;
+    float duration_ms;
+    int explicit;
+    String  artists;
+    String id_artists;
+    DATE release_date;
+    float danceability;
+    float energy;
+    int key;
+    float loudness;
+    int mode;
+    float speechiness;
+    float acousticness;
+    float instrumentalness;
+    float  liveness;
+    float  valence;
+    float tempo;
+    int time_signature;
+
+public:
+    list <string> listaArtists;
+    list <string> listaTracks;
+
+    //metodos
+
+
+};
+
+
+
+
+
+#endif //UNTITLED_ARQUIVOS_H
