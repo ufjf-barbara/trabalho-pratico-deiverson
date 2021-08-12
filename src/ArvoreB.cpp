841ca8f661cb1cac497501b6bf2c5a5f84af4edc
deyvisongdias <deyvisondias@hotmail.com>
2021-08-12 13:40:42 -0300

    add B tree

---
+++ src/ArvoreB.cpp
@@ -0,0 +1,22 @@
+#include "Artists.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <ctime>
+#include <algorithm>
+
+#include <utility>
+
+
+using namespace std;
+
+
+
+
+
+
---
+++ src/ArvoreB.h
@@ -0,0 +1,40 @@
+#include "Artists.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+#include <math.h>
+#include <ctime>
+#include <algorithm>
+
+#include <utility>
+
+#ifndef ARVOREB_H_INCLUDED
+#define ARVOREB_H_INCLUDED
+
+using namespace std;
+
+
+class ArvoreB
+{
+
+private:
+    Node* raiz;
+
+public:
+
+
+
+
+};
+
+
+
+
+
+
+
+#endif // ARVOREB_H_INCLUDED
